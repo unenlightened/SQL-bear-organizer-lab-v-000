2b6deaed7793e012317dce853cec907950d45f2b
Chely Ho <adept_inept@yahoo.com>
2018-03-02 03:06:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Mr. Chocolate", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Rowdy", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Tabitha", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Sergeant Brown", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Melissa", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Grinch", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Wendy", 6, M, 'brown', 'silky smooth', 'true');
+INSERT INTO bears (name, age, gender, color, temperament, alive ) VALUES ("Mr. Chocolate", 6, M, 'brown', 'silky smooth', 'true');
